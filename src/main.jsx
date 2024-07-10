70906268f4d56a5d5fd09909dcd4ea0d30bc6661
Gautam <pareekg531@gmail.com>
2024-07-10 20:17:43 +0530

    commited

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import {store} from "./store/Store.js";
+// import Store from './store/Store.js'
 import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById('root')).render(
 
